ae62fc8a8f9efbd0dbeaeabc22372aab2efeb9b0
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-23 00:01:23 -0700

    Square Array assignment completed.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  numbers = [1,2,3,9,10,16,25]
+  
+  numbers.map || numbers.collect ||  numbers.inject { |number| number ** 2 }
+
+  new_array = []
+  array.each { |number| new_array << number ** 2 }
+  new_array
 end
